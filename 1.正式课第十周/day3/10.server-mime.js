e2e71f54a70d178454f49430c900f8d548753a5d
zhufengpeixun <1144709265@qq.com>
2018-09-27 19:12:07 +0800

    12

---
+++ 1.正式课第十周/day3/10.server-mime.js
@@ -0,0 +1,16 @@
+let http = require("http");
+let fs = require("fs");
+let mime = require("mime");
+let  url = require("url");
+http.createServer(function (req,res) {
+    let  {pathname} = url.parse(req.url,true);
+    // mime ： 有个getType方法的返回值是当前文件对应的mime类型；
+    // 解决了不同的文件类型设置不同的响应头；
+    res.setHeader("Content-Type",mime.getType(pathname));
+    //console.log(mime.getType(pathname));
+    fs.readFile("."+pathname,function (err,data) {
+        res.end(data);
+    });
+}).listen(8080,function(){
+
+});
